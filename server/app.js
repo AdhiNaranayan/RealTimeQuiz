75516a123ad51e95f5391bbdec42b02df8afd9ab
Adhinarayanan P <adhinarayanan.p@pptssolutions.onmicrosoft.com>
2024-08-09 13:17:14 +0530

    Final Commit

--- server/app.js
+++ server/app.js
@@ -4,10 +4,19 @@ const connectDB = require('./config/db');
 const roomRoutes = require('./routes/roomRoutes');
 const gameRoutes = require('./routes/gameRoutes');
 const UserRoutes = require('./routes/userRoutes');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Route to serve the HTML page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 connectDB();
 
